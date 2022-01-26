8bdf6e95fc32e10d967a2b2fa8583086eee912b6
Nacho Martin <joseignacio.martinmoreno@gmail.com>
2022-01-26 20:17:50 +0100

    Initial Commit

---
+++ src/test/java/com/sanitas/calculator/service/CalculatorServiceMockTest.java
@@ -0,0 +1,29 @@
+package com.sanitas.calculator.service;
+
+import com.sanitas.calculator.domain.Request;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class CalculatorServiceMockTest {
+
+    @InjectMocks
+    CalculatorService calculatorService = new CalculatorServiceImpl();
+
+    @Test
+    void sum() {
+        int result = calculatorService.sum(new Request(1, 1));
+        assertThat(result).isEqualTo(2);
+    }
+
+
+    @Test
+    void subtraction() {
+        int result = calculatorService.subtraction(new Request(1, 1));
+        assertThat(result).isZero();
+    }
+}
